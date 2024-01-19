d82f453d3cb59aa1b44554070e85d34ac64803e4
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-20 00:35:18 +0900

    라이브러리 설치 항목 . 및 reset css App.js로 해당 컴포넌트 배치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import React from "react";
+import { Suspense } from "react";
+import { Loader } from "./Loader";
+import { artist, artist2 } from "./Poster/artistDummy.js";
+
+const Title = React.lazy(() => import("./Title/Title.jsx"));
+const Program = React.lazy(() => import("./Program/Program.jsx"));
+const ArtistView = React.lazy(() => import("./Artist/View.jsx"));
+const PosterView = React.lazy(() => import("./Poster/View.jsx"));
+const MapView = React.lazy(() => import("./Map/View.jsx"));
 function App() {
-  return <div className="App">asd</div>;
+  //useEffect는 컴포넌트가 렌더링 될 때마다 작업을 실행할 수 있도록 하는 Hook
+  // 만약 []를 넣으면 최초 1번 렌더링될 때 실행
+  // []를 넣지 않으면 리렌더링할때마다 실행
+  // 만약 특정 값이 업데이트 될 때 실행하고 싶다면 []에 해당 업데이트 변수를 넣어주면된다.
+
+  return (
+    <div className="App">
+      <Suspense fallback={<Loader />}>
+        <Title />
+      </Suspense>
+      <Suspense>
+        <Program />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <ArtistView />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <PosterView artist={artist} time={"15:00"} />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <PosterView artist={artist2} time={"19:00"} />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <MapView />
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
--- src/Map/View.jsx
+++ src/Map/View.jsx
@@ -1,46 +1,77 @@
+import { useInView } from "react-intersection-observer";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import styled, { keyframes } from "styled-components";
+import flower from "./flower.png";
 
-export const MapView = () => {
+const frameInAnimation = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-20%);
+  }
+
+  100%{
+    opacity: 1;
+    transform: translateY(0%);
+  }
+`;
+const Fake = styled.div`
+  background-color: #f7e1e2;
+  z-index: -1;
+`;
+
+const Container = styled.div`
+  animation: ${(props) => (props.inView ? frameInAnimation : null)} 2s forwards;
+`;
+const MarkerText = styled.div`
+  background-color: #ffe4e1;
+  border: 0;
+  border-radius: 10px;
+  padding: 10px;
+  font-weight: 400;
+`;
+
+export default function MapView() {
   const findDirectionToLieto = () => {
     window.open(
       `https://map.kakao.com/link/to/광주디엠홀,${process.env.REACT_APP_LIETO_LAT},${process.env.REACT_APP_LIETO_LNG}`
     );
   };
 
+  const { ref, inView, entry } = useInView({ threshold: 0.1 });
+
   return (
-    <>
-      <Map
-        center={{
-          lat: process.env.REACT_APP_LIETO_LAT,
-          lng: process.env.REACT_APP_LIETO_LNG,
-        }}
-        style={{ width: "100%", height: "360px" }}
-        level={2}
-      >
-        <MapMarker
-          position={{
+    <Fake>
+      <Container ref={ref} inView={inView}>
+        <Map
+          center={{
             lat: process.env.REACT_APP_LIETO_LAT,
             lng: process.env.REACT_APP_LIETO_LNG,
           }}
+          style={{ width: "100%", height: "360px" }}
+          level={2}
         >
-          <div style={{ color: "#000" }}>광주 디엠홀</div>
-        </MapMarker>
-      </Map>
-      <button onClick={findDirectionToLieto}>길찾기</button>
-    </>
+          <MapMarker
+            onClick={findDirectionToLieto}
+            position={{
+              lat: process.env.REACT_APP_LIETO_LAT,
+              lng: process.env.REACT_APP_LIETO_LNG,
+            }}
+            image={{
+              src: flower,
+              size: {
+                width: 50,
+                height: 50,
+              }, // 마커이미지의 크기입니다
+              options: {
+                offset: {
+                  x: 27,
+                  y: 69,
+                }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+              },
+            }}
+          ></MapMarker>
+        </Map>
+      </Container>
+    </Fake>
   );
-};
-/* const [gps, setGps] = useState([37.402056, 127.108212]);
-
-  const getUserGps = () => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((position) =>
-        resolve([position.coords.latitude, position.coords.longitude])
-      );
-    });
-  };
-
-  useEffect(async () => {
-    const [lat, lng] = await getUserGps();
-    setGps([lat, lng]);
-  }, []);*/
+}
