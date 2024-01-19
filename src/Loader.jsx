8800c48680af13b7651c969da421968e7e9a50ff
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-20 00:34:17 +0900

    feat: Suspense하는 동안 보여줄 Loader 개발

---
+++ src/Loader.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Spinner from "./spinner.gif";
+const Container = styled.div`
+  width: 100vh;
+  height: 100ch;
+  background-color: #ffe4e1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Mention = styled.h2`
+  text-align: center;
+`;
+
+export const Loader = () => {
+  return (
+    <Container>
+      <Mention>잠시만 기다려주세요</Mention>
+      <img src={Spinner} alt="로딩" width="10%"></img>
+    </Container>
+  );
+};
