0422857d863c3f63983a674a328eff7aa54db1d5
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-20 19:55:52 +0900

    style: map draggable

--- src/Map/View.jsx
+++ src/Map/View.jsx
@@ -62,7 +62,7 @@ export default function MapView({ where, gps, title, detail }) {
           }}
           style={{ width: "100%", height: "300px" }}
           level={2}
-          draggable={false}
+          draggable={true}
         >
           <CustomOverlayMap
             position={{
