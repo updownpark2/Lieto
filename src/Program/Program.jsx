9d567c4a7e3190daf3b8a82a2d36b9afb1c4117f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-22 16:42:06 +0900

    곡변경 및 팜플렛 추가

--- src/Program/Program.jsx
+++ src/Program/Program.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import Poster from "./LietoPoster.jpeg";
+import Pamplet from "./LietoPam1.jpeg";
+import Pamplet2 from "./LietoPam2.jpeg";
 
 const Container = styled.div`
   display: flex;
   justify-content: space-around;
+  flex-direction: column;
   align-items: center;
   background-color: #f7e1e2;
   height: 100vh;
@@ -28,6 +31,8 @@ export default function Program() {
   return (
     <Container>
       <PosterImage src={Poster}></PosterImage>
+      <PosterImage src={Pamplet}></PosterImage>
+      <PosterImage src={Pamplet2}></PosterImage>
     </Container>
   );
 }
